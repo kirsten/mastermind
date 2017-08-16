0d26738e089a52ff69edc85e81e9b0669010d035
Kirsten Nordine <kirstennordine@gmail.com>
2017-08-16 17:24:26 -0400

    Initial commit

---
+++ src/Game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <header>
+          <h1>Mastermind</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Game;
