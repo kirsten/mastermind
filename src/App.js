24641a11c220b14f0a5aa0c94744b9295dbafaa2
Kirsten Nordine <kirstennordine@gmail.com>
2017-08-24 19:19:08 -0400

    Code pegs can be selected and added to a blank row
    - Remove `DecodingBoard` component - not sure this is a useful
      abstraction yet.
    - Reduce # of rows to 1 for now to simplify development.
    - Add different colors of code pegs underneath the decoding board.
    - Start keeping track of a few things in App state:
      - Code peg color choices (used to render code pegs underneath the
        board)
      - Active code peg (when an empty peg hole is clicked on the board, it
        becomes "active", allowing an additional click of a code peg
        underneath to add a color to the active peg hole)
      - Guess - just an array of 4 color values which will renders the
        differently colored pegs in a row on the board.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,83 @@ import './App.css';
 class CodePeg extends Component {
   render() {
     return (
-      <div className="code-peg"></div>
+      <div
+        className={`code-peg ${this.props.active ? 'active' : ''}`}
+        style={{backgroundColor: this.props.color}}
+        onClick={() => this.props.onClick()}
+      ></div>
     );
   }
 }
 
-class DecodingBoard extends Component {
-  renderCodePegs() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      codePegChoices: ["fuchsia", "red", "orange", "yellow", "lime", "aqua"],
+      guess: Array(4).fill(null),
+      activeCodePeg: null
+    };
+  }
+
+  renderCodePegChoices() {
     let pegs = []
-    for(let i = 0; i < 4; i++) {
-      let peg = <CodePeg key={i} />
+    for(let i = 0; i < this.state.codePegChoices.length; i++) {
+      let color = this.state.codePegChoices[i];
+      let peg = (
+        <CodePeg
+          key={i}
+          color={color}
+          onClick={() => this.handleCodePegSelection(i)}
+        />
+      );
       pegs.push(peg)
     }
     return pegs;
   }
 
+  renderCodePegHoles() {
+    let holes = []
+    for(let i = 0; i < this.state.guess.length; i++) {
+      let hole = (
+        <CodePeg
+          key={i}
+          active={this.state.activeCodePeg === i}
+          color={this.state.guess[i]}
+          onClick={() => this.toggleActiveCodePeg(i)}
+        />
+      );
+      holes.push(hole)
+    }
+    return holes;
+  }
+
+  toggleActiveCodePeg(i) {
+    if (this.state.activeCodePeg === i) {
+      this.setState({activeCodePeg: null})
+    } else {
+      this.setState({activeCodePeg: i})
+    }
+  }
+
+  handleCodePegSelection(colorIndex) {
+    let color = this.state.codePegChoices[colorIndex];
+    if (this.state.activeCodePeg === null) {
+      return;
+    } else {
+      let guess = this.state.guess;
+      guess.splice(this.state.activeCodePeg, 1, color);
+      let pegChoices = this.state.codePegChoices;
+      pegChoices.splice(colorIndex, 1);
+      this.setState({guess: guess});
+      this.setState({activeCodePeg: null});
+    }
+  }
+
   renderRows() {
     let rows = []
-    for(let i = 0; i < 10; i++) {
-      let row = <div key={i} className="board-row">{this.renderCodePegs()}</div>
+    for(let i = 0; i < 1; i++) {
+      let row = <div key={i} className="board-row">{this.renderCodePegHoles()}</div>
       rows.push(row)
     }
     return rows;
@@ -30,19 +88,12 @@ class DecodingBoard extends Component {
 
   render() {
     return (
-      <div className="decoding-board">{this.renderRows()}</div>
-    );
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
       <div className="game">
         <header>
           <h1>Mastermind</h1>
         </header>
-        <DecodingBoard/>
+        <div className="decoding-board">{this.renderRows()}</div>
+        <div className="code-peg-selection">{this.renderCodePegChoices()}</div>
       </div>
     );
   }
